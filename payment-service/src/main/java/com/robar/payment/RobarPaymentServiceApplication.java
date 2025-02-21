8677c6d1df2251dfe245533fd8c761f953127f2a
Hans867 <hans867m@stud.kea.dk>
2025-02-21 17:28:41 +0100

    testing

--- payment-service/src/main/java/com/robar/payment/RobarPaymentServiceApplication.java
+++ payment-service/src/main/java/com/robar/payment/RobarPaymentServiceApplication.java
@@ -3,13 +3,18 @@ package com.robar.payment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan("com.robar.payment.config")
+@ComponentScan(basePackages = {
+    "com.robar.payment",
+    "com.robar.payment.controller",
+    "com.robar.payment.service",
+    "com.robar.payment.config"
+})
 public class RobarPaymentServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(RobarPaymentServiceApplication.class, args);
     }
-} 
-
-
+}
